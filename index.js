11013425d9861b43b51a48d7e6c9075903bf2b51
jahangir <jahangiralomtuf99@gmail.com>
2024-03-31 23:38:57 +0600

    [UPDATE] - added jwt and get methord

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const httpStatus = require("http-status");
+const jwt = require("jsonwebtoken");
 
 const port = process.env.PORT || 8000;
 
@@ -13,15 +14,17 @@ app.use(cors());
 // body pars
 app.use(express.json());
 
-// User and password
+// User and password for assignment purposes
 // task-management
 // tloJmLqMjRxXMelU
 
+// Secret key for jwt authorization , this file will only be in env file. I did not put it for Assignment purposes.
+const secretKey =
+  "fd69d36be8184e89943729e9a70ec4905fd602c4b4ddf4ca4a1a1fdae6c6102be3e407ca704d8b1ff8e2df200ab742ddb40b82279b7b9f336947ce9709a3b489";
 
-// MongoBD Connection URL 
-const uri = "mongodb+srv://task-management:tloJmLqMjRxXMelU@cluster0.e9wqxpd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-
+// MongoBD Connection URL
+const uri =
+  "mongodb+srv://task-management:tloJmLqMjRxXMelU@cluster0.e9wqxpd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Database Connection
 const client = new MongoClient(uri, {
@@ -29,27 +32,57 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 // Run Function
 async function run() {
   try {
-      await client.connect();
-      
+    // connect to Database
+    const taskCollection = client.db("task-DB").collection("task-listing");
+    // Connect to client server
+    await client.connect();
+
+    // Middleware for verifying token
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "forbidden access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    // Create JWT authorization
+    app.post("/api/v1/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, secretKey, { expiresIn: "1h" });
+      res.send({ token });
+    });
+
+    // Get api For task Listing
+    app.get("/api/v1/task-listing", async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    });
+
+    // ---------------------------------------------------end----------------------------
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-
 
 // Route call
 app.get("/", (req, res) => {
@@ -59,19 +92,18 @@ app.get("/", (req, res) => {
 });
 
 // Handle Not Found Route
-app.use((req, res, next) => {
-  res.status(httpStatus.NOT_FOUND).json({
-    success: false,
-    message: "Not Found",
-    errorMessage: [
-      {
-        path: req.originalUrl,
-        message: "API NOT FOUND",
-      },
-    ],
-  });
-  next();
-});
+// app.use((req, res) => {
+//   res.status(httpStatus.NOT_FOUND).json({
+//     success: false,
+//     message: "Not Found",
+//     errorMessage: [
+//       {
+//         path: req.originalUrl,
+//         message: "API NOT FOUND",
+//       },
+//     ],
+//   });
+// });
 
 // Start Server
 app.listen(port, () => {
