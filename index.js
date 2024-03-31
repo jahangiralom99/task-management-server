355e009ef300014073a7c70c826da26f331eb8ee
jahangir <jahangiralomtuf99@gmail.com>
2024-03-31 06:42:45 +0600

    [FEAT] - add mongobd connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const httpStatus = require("http-status");
 
 const port = process.env.PORT || 8000;
@@ -11,6 +12,44 @@ app.use(cors());
 
 // body pars
 app.use(express.json());
+
+// User and password
+// task-management
+// tloJmLqMjRxXMelU
+
+
+// MongoBD Connection URL 
+const uri = "mongodb+srv://task-management:tloJmLqMjRxXMelU@cluster0.e9wqxpd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+
+// Database Connection
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// Run Function
+async function run() {
+  try {
+      await client.connect();
+      
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 // Route call
 app.get("/", (req, res) => {
